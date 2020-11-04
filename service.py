bda1c9e082121ad57a62201049a5f97408e79aba
Geir Ove Grønmo <grove@geirove.org>
2020-11-04 18:04:25 +0100

    Using core api to list nodes

--- service.py
+++ service.py
@@ -37,6 +37,9 @@ with open(os.path.join(homedir, "channel")) as f:
 with open(os.path.join(homedir, "token")) as f:
     token = f.read().strip()
 
+subscription_id = os.environ["SUBSCRIPTION_ID"]
+skip_push = os.environ.get("SKIP_PUSH") is not None
+
 logger.info("Starting")
 status = {}
 
@@ -44,45 +47,49 @@ while True:
     try:
         c = client.CoreV1Api()
 
-        for event in watch.Watch().stream(c.list_node, timeout_seconds=30):
-            raw_object = event["raw_object"]
-            # print(json.dumps(raw_object, indent=2))
-            event_type = event['type']
-            node_name = raw_object["metadata"]["name"]
+        for node in c.list_node().items:
+            node_name = node.metadata.name
             conditions = {}
-            for condition in raw_object["status"]["conditions"]:
-                conditions[condition["type"]] = condition["status"]
+            for condition in node.status.conditions:
+                conditions[condition.type] = condition.status
         
             old_conditions = status.get(node_name)
-            if old_conditions is not None and old_conditions != conditions:
-                logger.info(f"Name: {node_name} Event: {event_type} {json.dumps(conditions, indent=2)}")
+            if old_conditions is None:
+                logger.info(f"Subscription: {subscription_id} Node: {node_name} State: {json.dumps(conditions, indent=2)}")
+            elif old_conditions != conditions:
+                logger.info(f"Subscription: {subscription_id} Node: {node_name} State: {json.dumps(conditions, indent=2)}")
 
                 
                 url = "https://slack.com/api/chat.postMessage"
                 data = {
                     "channel": channel,
-                    "text": f"Name: {node_name} Event: {event_type} {json.dumps(conditions, indent=2)}",
+                    "text": f"Subscription: *{subscription_id}*\nNode: {node_name}\nState: {json.dumps(conditions, indent=2)}",
                     "username": "kubeslack"
                 }
                 logger.info(data)
 
-                for i in range(0, 5):
-                    rd = requests.post(url,
-                                       headers={
-                                           "Authorization": f"Bearer {token}",
-                                           "Content-type": "application/json; charset=utf-8"
-                                       },
-                                       data=json.dumps(data),
-                                       timeout=(10, 30))
-                    if rd.status_code == 200:
-                        logger.info(f"Posted message to slack: {node_name}")
-                        break
-                    else:
-                        logger.error(f"Not able to post message to slack: {rd.status_code} {rd.text}")
-                        
-                    time.sleep(5)
-                
+                if skip_push:
+                    logger.info(f"Posted message to slack (ignored): {node_name}")
+                else:
+                    for i in range(0, 5):
+                        rd = requests.post(url,
+                                           headers={
+                                               "Authorization": f"Bearer {token}",
+                                               "Content-type": "application/json; charset=utf-8"
+                                           },
+                                           data=json.dumps(data),
+                                           timeout=(10, 30))
+                        if rd.status_code == 200:
+                            logger.info(f"Posted message to slack: {node_name}")
+                            break
+                        else:
+                            logger.error(f"Not able to post message to slack: {rd.status_code} {rd.text}")
+                            
+                        time.sleep(5)
+                    
             status[node_name] = conditions
+
+        time.sleep(10)
             
     except KeyboardInterrupt:
         raise
